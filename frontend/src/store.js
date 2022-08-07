7b346a849bfe2f216e9bf8c211a4f00fb77086aa
sammysundar2001@gmail.com <samradnus2001@gmail.com>
2022-08-07 22:27:36 +0530

    Added Redux for Products

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,11 @@
 import {createStore,combineReducers,applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-
-const reducer=combineReducers({})
+import { productListReducers,productDetailsReducers } from './reducers/productReducers'
+const reducer=combineReducers({
+    productList:productListReducers,
+    productDetails:productDetailsReducers,
+})
 const initialState={}
 const middleware=[thunk]
 const store=createStore(reducer,initialState,composeWithDevTools(applyMiddleware(...middleware)))
