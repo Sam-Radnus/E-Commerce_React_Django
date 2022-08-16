0d2a991aadc571cc16e9fdfef19f8a1af4610e48
sammysundar2001@gmail.com <samradnus2001@gmail.com>
2022-08-16 17:34:22 +0530

    added PlaceOrderScreen

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { register } from '../actions/userActions'
 import FormContainer from '../components/FormContainer'
+import CheckoutState from '../components/CheckoutState'
 import { saveShippingAddress} from '../actions/cartActions'
 const ShippingScreen = () => {
     const navigate=useNavigate();
@@ -26,6 +27,7 @@ const ShippingScreen = () => {
     }
     return (
     <FormContainer>
+        <CheckoutState step1 step2/>
         <h1>Shipping</h1>
         <Form onSubmit={submitHandler}>
         <Form.Group controlId='address'>
