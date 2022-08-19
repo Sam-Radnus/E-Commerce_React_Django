c217843c1738027f6abf6d4f0c4fff993646c142
sammysundar2001@gmail.com <samradnus2001@gmail.com>
2022-08-19 08:54:41 +0530

    Added Order Details

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,5 +1,23 @@
 import axios from "axios"
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, CART_CLEAR_ITEMS, ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS } from "../constants/orderConstants";
+import { ORDER_CREATE_FAIL,
+         ORDER_CREATE_REQUEST,
+         ORDER_CREATE_SUCCESS, 
+      
+         CART_CLEAR_ITEMS, 
+
+         ORDER_DETAILS_FAIL,
+         ORDER_DETAILS_REQUEST,
+         ORDER_DETAILS_SUCCESS,
+         
+         ORDER_PAY_REQUEST,
+         ORDER_PAY_SUCCESS,
+         ORDER_PAY_FAIL,
+         ORDER_PAY_RESET,
+         ORDER_LIST_MY_REQUEST,
+         ORDER_LIST_MY_SUCCESS,
+         ORDER_LIST_MY_FAIL,
+         ORDER_LIST_MY_RESET,
+    } from "../constants/orderConstants";
 
 export const createOrder = (order) => async (dispatch, getState) => {
     try {
@@ -43,7 +61,7 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
         const config = {
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjU4OTQyODE1LCJpYXQiOjE2NTg5NDI1MTUsImp0aSI6ImY1NDU3NTQ1MzE3ODQxMTM4NTk5MTcxYTRmNjQ1Nzk5IiwidXNlcl9pZCI6MTEsInVzZXJuYW1lIjoiRGF2aWQgTm9ydGgifQ.7K7QaubU7DvcL7fh7jE4bNadQPh81VEneHZ0--QiK44`
+                'Authorization': `Bearer ${userInfo.token}`
             }
         }
         console.log(3);
@@ -59,6 +77,66 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
     catch (error) {
         dispatch({
             type: ORDER_DETAILS_FAIL,
+            payload: error.response && error.response.data.detail ? error.response.data.detail : error.message
+        })
+    }
+}
+export const payOrder = (id,paymentResult) => async (dispatch, getState) => {
+    try {
+        dispatch({ type: ORDER_PAY_REQUEST })
+        const { userLogin: { userInfo } } = getState();
+        console.log(1);
+
+        console.log(userInfo);
+        console.warn(userInfo.token)
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${userInfo.token}`
+            }
+        }
+        console.log(3);
+        const { data } = await axios.put(`/api/orders/${id}/pay`,paymentResult, config)
+
+        dispatch({
+            type: ORDER_PAY_SUCCESS,
+            loading:false,
+            payload: data
+        })
+        console.log(4);
+    }
+    catch (error) {
+        dispatch({
+            type: ORDER_PAY_FAIL,
+            payload: error.response && error.response.data.detail ? error.response.data.detail : error.message
+        })
+    }
+}
+export const getMyOrders = () => async (dispatch, getState) => {
+    try {
+        dispatch({ type: ORDER_LIST_MY_REQUEST })
+        const { userLogin: { userInfo } } = getState();
+        console.log(1);
+
+        console.log(userInfo);
+        console.warn(userInfo.token)
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${userInfo.token}`
+            }
+        }
+        console.log(3);
+        const { data } = await axios.get(`/api/orders/myorders/`, config)
+        dispatch({
+            type: ORDER_LIST_MY_SUCCESS,
+            loading:false,
+            payload: data
+        })
+    }
+    catch (error) {
+        dispatch({
+            type: ORDER_LIST_MY_FAIL,
             payload: error.response && error.response.data.detail ? error.response.data.detail : error.message
         })
     }
